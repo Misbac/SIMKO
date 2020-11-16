a264f8f8515f564bb32a8015ad922f461a6bcb4b
Wulung Wahyu Witono <wulungqwerty@gmail.com>
2020-11-16 23:32:41 +0700

    upadate wulung

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/homepage', function () {
+    return view('homepage.hmpg');
+});
+Route::get('/anggota', function () {
+    return view('anggota.anggota');
+});
+Route::get('/profil', function () {
+    return view('profil.profil');
+});
